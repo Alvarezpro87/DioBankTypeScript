17aabb4f01268ca641bf0a0bbb79ecbf9041cce4
Alvarezpro87 <alvareznuvem@gmail.com>
2024-07-08 10:13:36 -0300

    Acrescentando separações das contas no Appjs

--- app.ts
+++ app.ts
@@ -4,14 +4,17 @@ import { PeopleAccount } from './class/PeopleAccount'
 import { WorkerAccount } from './class/WorkerAccount'
 
 const peopleAccount: PeopleAccount = new PeopleAccount(1, 'Nath', 10)
+console.log("=======================  PeopleAccount  =======================");
 console.log(peopleAccount)
 peopleAccount.deposit(100)
 
 const companyAccount: CompanyAccount = new CompanyAccount('DIO', 20)
+console.log("=======================  CompanyAccount  =======================");
 companyAccount.deposit(100)
 companyAccount.getLoan(100)
 console.log(companyAccount)
 
 const specialAccount: WorkerAccount = new WorkerAccount('ALex', 40)
+console.log("=======================  WorkerAccount  =======================");
 specialAccount.deposit(120)
 console.log(specialAccount);
